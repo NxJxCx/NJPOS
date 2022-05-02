a1176e03bdaca5a027fe996ab48302ee55811f95
Neil Jason Cañete <caneteneiljason@gmail.com>
2022-05-02 17:33:09 +0800

    pos system c

---
+++ pos.c
@@ -0,0 +1,29 @@
+/**
+ * @file pos.c
+ * @author Neil Jason Cañete (caneteneiljason@smccnasipit.edu.ph)
+ * @date 2022-05-02
+ * 
+ * @copyright Copyright (c) Neil Jason Cañete @ SMCC 2022
+ * @brief 
+ * CREATE A POINT OF SALE (POS) SYSTEM.
+ * The program should must these following major features:
+ * > (Add, Display, Search, Update and Delete) Product details
+ * > (Add, Display, Search, Update and Delete) Teller details
+ * > Sale transaction
+ * # PRODUCT DETAILS # should accept these following data:
+ * > Product ID (should be unique)
+ * > Product Name
+ * > Product Description (this would describe the product)
+ * > Product Category (example: dairy products, soft drinks, hygiene products etc.)
+ * > Product Unit (piece or kilo)
+ * > Product Unit Price
+ * # TELLER DETAILS # should accept these following data:
+ * > Teller ID (should be unique)
+ * > Teller Name
+ * # SALE TRANSACTION # should accept these following data:
+ * > Sale ID (should be unique)
+ * > Product Name
+ * > Product Unit
+ * > Product Price
+ * > Quantity
+ */
